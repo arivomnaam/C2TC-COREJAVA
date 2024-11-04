eae6a5a5c0fe1dfd7acef42b189b891048332fbe
smart <smart@SMART>
2024-11-04 15:31:17 +0530

    Scanner, Encapsulation and Constructor

---
+++ C2TC_DHEVENDHIRAN/src/com/tns/ifet/daythree/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,13 @@
+package com.tns.ifet.daythree.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		OopsConceptDemo obj = new OopsConceptDemo(); 
+		obj.setSerialNum(100); 
+		obj.setName("Dhevendhiran.R"); 
+		obj.setAge(21); 
+		System.out.println(obj); 
+	}
+
+}
